978b9e5ec2e85774bce14df519ebd30757362b8e
Ken Stipek <ken@brivity.com>
2014-12-17 23:57:07 -0800

    Set env vars.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "RPkrq7WFsnc6mlXJqdniYxw2z"
-  config.consumer_secret     = "QfuZDRnn1yoGF7RBWdeTnNKTSOUngASxi8OtfnT2bDOQDrmTlf"
-  config.access_token        = "432171841-oTZp65Q8MA9tOHZu64KELCNKjXcwg3LyiHohWhDW"
-  config.access_token_secret = "5Jquv0fJNgc7gDRtJm44QpqFvYxxogUZXqztwK34EAjfg"
+  config.consumer_key        = ENV['consumer_key']
+  config.consumer_secret     = ENV['consumer_secret']
+  config.access_token        = ENV['access_token']
+  config.access_token_secret = ENV['access_token_secret']
 end
